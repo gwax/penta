62671cf7e0b86e6c74cb325d70f5019514a5f54c
George Leslie-Waksman <george@manapool.com>
2026-08-22 10:06:38 -0700

    Implement Guardian Angel with ongoing effects

--- src/card/sets/y1994/revised.rs
+++ src/card/sets/y1994/revised.rs
@@ -3,7 +3,10 @@
 //! Cards legal through this printing reuse their earliest built-in definition.
 
 use super::{CardRecord, PrintingRecord};
+use crate::card::sets::y1993::alpha;
 
 pub(in crate::card::sets) static CARDS: &[&CardRecord] = &[];
 
-pub(in crate::card::sets) static ADDITIONAL_PRINTINGS: &[PrintingRecord] = &[];
+pub(in crate::card::sets) static ADDITIONAL_PRINTINGS: &[PrintingRecord] = &[
+    PrintingRecord::reprint(&alpha::GUARDIAN_ANGEL), // 3ED 21
+];
