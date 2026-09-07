eb2ecb920262926bccc9f1b266a712d98b5377cb
George Leslie-Waksman <george@manapool.com>
2026-09-07 00:23:08 -0700

    Migrate upstream Phyrexian Plaguelord to shared sacrifice costs

--- src/card/sets/y1999/urzas_legacy.rs
+++ src/card/sets/y1999/urzas_legacy.rs
@@ -1102,9 +1102,9 @@ pub(in crate::card::sets) static PHYREXIAN_PLAGUELORD: CardRecord = CardRecord::
             ),
             AbilityDef::activated_with_targets(
                 "Sacrifice a creature: Target creature gets -1/-1 until end of turn.",
-                &[CostDef::SacrificePermanent {
+                &[CostDef::Sacrifice {
                     object: ObjectPredicateDef::HasType(CardType::Creature),
-                    controller: PlayerRelation::You,
+                    quantity: CostQuantityDef::Fixed(1),
                 }],
                 &const {
                     [AbilityTargetDef::exactly_one_permanent(
